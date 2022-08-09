9233ef9fcc1e61fe15c5c5201d071838062ab4e5
MarcoBonafede <marcobonafede@hotmail.com>
2022-08-09 02:57:28 -0300

    Base preliminar del formulario

---
+++ AnalisisNumerico-BonafedeMarengo/Formulario.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace AnalisisNumerico_BonafedeMarengo
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxEntradasU1 = new System.Windows.Forms.GroupBox();
+            this.txtMaxIteracionesU1 = new System.Windows.Forms.TextBox();
+            this.txtToleranciaU1 = new System.Windows.Forms.TextBox();
+            this.txtXDerechaU1 = new System.Windows.Forms.TextBox();
+            this.txtXIzquierdaU1 = new System.Windows.Forms.TextBox();
+            this.txtFuncionU1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbxSalidasU1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.gbxMetodosU1 = new System.Windows.Forms.GroupBox();
+            this.txtConvergeU1 = new System.Windows.Forms.TextBox();
+            this.txtErrorRelatU1 = new System.Windows.Forms.TextBox();
+            this.txtRaizU1 = new System.Windows.Forms.TextBox();
+            this.txtCantIteracionesU1 = new System.Windows.Forms.TextBox();
+            this.btnBiseccionU1 = new System.Windows.Forms.Button();
+            this.btnReglaFalsaU1 = new System.Windows.Forms.Button();
+            this.btnTangenteU1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.gbxEntradasU1.SuspendLayout();
+            this.gbxSalidasU1.SuspendLayout();
+            this.gbxMetodosU1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxEntradasU1
+            // 
+            this.gbxEntradasU1.Controls.Add(this.txtMaxIteracionesU1);
+            this.gbxEntradasU1.Controls.Add(this.txtToleranciaU1);
+            this.gbxEntradasU1.Controls.Add(this.txtXDerechaU1);
+            this.gbxEntradasU1.Controls.Add(this.txtXIzquierdaU1);
+            this.gbxEntradasU1.Controls.Add(this.txtFuncionU1);
+            this.gbxEntradasU1.Controls.Add(this.label5);
+            this.gbxEntradasU1.Controls.Add(this.label4);
+            this.gbxEntradasU1.Controls.Add(this.label3);
+            this.gbxEntradasU1.Controls.Add(this.label2);
+            this.gbxEntradasU1.Controls.Add(this.label1);
+            this.gbxEntradasU1.Location = new System.Drawing.Point(12, 12);
+            this.gbxEntradasU1.Name = "gbxEntradasU1";
+            this.gbxEntradasU1.Size = new System.Drawing.Size(760, 220);
+            this.gbxEntradasU1.TabIndex = 0;
+            this.gbxEntradasU1.TabStop = false;
+            this.gbxEntradasU1.Text = "Entradas";
+            this.gbxEntradasU1.Visible = false;
+            // 
+            // txtMaxIteracionesU1
+            // 
+            this.txtMaxIteracionesU1.Location = new System.Drawing.Point(308, 81);
+            this.txtMaxIteracionesU1.Name = "txtMaxIteracionesU1";
+            this.txtMaxIteracionesU1.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxIteracionesU1.TabIndex = 9;
+            // 
+            // txtToleranciaU1
+            // 
+            this.txtToleranciaU1.Location = new System.Drawing.Point(308, 17);
+            this.txtToleranciaU1.Name = "txtToleranciaU1";
+            this.txtToleranciaU1.Size = new System.Drawing.Size(100, 20);
+            this.txtToleranciaU1.TabIndex = 8;
+            // 
+            // txtXDerechaU1
+            // 
+            this.txtXDerechaU1.Location = new System.Drawing.Point(49, 145);
+            this.txtXDerechaU1.Name = "txtXDerechaU1";
+            this.txtXDerechaU1.Size = new System.Drawing.Size(100, 20);
+            this.txtXDerechaU1.TabIndex = 7;
+            // 
+            // txtXIzquierdaU1
+            // 
+            this.txtXIzquierdaU1.Location = new System.Drawing.Point(49, 81);
+            this.txtXIzquierdaU1.Name = "txtXIzquierdaU1";
+            this.txtXIzquierdaU1.Size = new System.Drawing.Size(100, 20);
+            this.txtXIzquierdaU1.TabIndex = 6;
+            // 
+            // txtFuncionU1
+            // 
+            this.txtFuncionU1.Location = new System.Drawing.Point(49, 17);
+            this.txtFuncionU1.Name = "txtFuncionU1";
+            this.txtFuncionU1.Size = new System.Drawing.Size(100, 20);
+            this.txtFuncionU1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Xi = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Xd = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tolerancia: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Iteraciones Máx.:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F (x) =";
+            // 
+            // gbxSalidasU1
+            // 
+            this.gbxSalidasU1.Controls.Add(this.txtCantIteracionesU1);
+            this.gbxSalidasU1.Controls.Add(this.txtRaizU1);
+            this.gbxSalidasU1.Controls.Add(this.txtErrorRelatU1);
+            this.gbxSalidasU1.Controls.Add(this.txtConvergeU1);
+            this.gbxSalidasU1.Controls.Add(this.label9);
+            this.gbxSalidasU1.Controls.Add(this.label8);
+            this.gbxSalidasU1.Controls.Add(this.label7);
+            this.gbxSalidasU1.Controls.Add(this.label6);
+            this.gbxSalidasU1.Location = new System.Drawing.Point(12, 238);
+            this.gbxSalidasU1.Name = "gbxSalidasU1";
+            this.gbxSalidasU1.Size = new System.Drawing.Size(760, 220);
+            this.gbxSalidasU1.TabIndex = 1;
+            this.gbxSalidasU1.TabStop = false;
+            this.gbxSalidasU1.Text = "Salidas";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(4, 188);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Raíz (Xr) = ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(4, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Error Relat.:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Cant. Iterac.:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Converge:";
+            // 
+            // gbxMetodosU1
+            // 
+            this.gbxMetodosU1.Controls.Add(this.button4);
+            this.gbxMetodosU1.Controls.Add(this.btnTangenteU1);
+            this.gbxMetodosU1.Controls.Add(this.btnReglaFalsaU1);
+            this.gbxMetodosU1.Controls.Add(this.btnBiseccionU1);
+            this.gbxMetodosU1.Location = new System.Drawing.Point(778, 12);
+            this.gbxMetodosU1.Name = "gbxMetodosU1";
+            this.gbxMetodosU1.Size = new System.Drawing.Size(147, 446);
+            this.gbxMetodosU1.TabIndex = 2;
+            this.gbxMetodosU1.TabStop = false;
+            this.gbxMetodosU1.Text = "Métodos";
+            // 
+            // txtConvergeU1
+            // 
+            this.txtConvergeU1.Enabled = false;
+            this.txtConvergeU1.Location = new System.Drawing.Point(76, 13);
+            this.txtConvergeU1.Name = "txtConvergeU1";
+            this.txtConvergeU1.Size = new System.Drawing.Size(100, 20);
+            this.txtConvergeU1.TabIndex = 4;
+            // 
+            // txtErrorRelatU1
+            // 
+            this.txtErrorRelatU1.Enabled = false;
+            this.txtErrorRelatU1.Location = new System.Drawing.Point(76, 129);
+            this.txtErrorRelatU1.Name = "txtErrorRelatU1";
+            this.txtErrorRelatU1.Size = new System.Drawing.Size(100, 20);
+            this.txtErrorRelatU1.TabIndex = 5;
+            // 
+            // txtRaizU1
+            // 
+            this.txtRaizU1.Enabled = false;
+            this.txtRaizU1.Location = new System.Drawing.Point(76, 185);
+            this.txtRaizU1.Name = "txtRaizU1";
+            this.txtRaizU1.Size = new System.Drawing.Size(100, 20);
+            this.txtRaizU1.TabIndex = 6;
+            // 
+            // txtCantIteracionesU1
+            // 
+            this.txtCantIteracionesU1.Enabled = false;
+            this.txtCantIteracionesU1.Location = new System.Drawing.Point(76, 73);
+            this.txtCantIteracionesU1.Name = "txtCantIteracionesU1";
+            this.txtCantIteracionesU1.Size = new System.Drawing.Size(100, 20);
+            this.txtCantIteracionesU1.TabIndex = 7;
+            // 
+            // btnBiseccionU1
+            // 
+            this.btnBiseccionU1.Location = new System.Drawing.Point(7, 13);
+            this.btnBiseccionU1.Name = "btnBiseccionU1";
+            this.btnBiseccionU1.Size = new System.Drawing.Size(75, 23);
+            this.btnBiseccionU1.TabIndex = 0;
+            this.btnBiseccionU1.Text = "Bisección";
+            this.btnBiseccionU1.UseVisualStyleBackColor = true;
+            this.btnBiseccionU1.Click += new System.EventHandler(this.btnBiseccionU1_Click);
+            // 
+            // btnReglaFalsaU1
+            // 
+            this.btnReglaFalsaU1.Location = new System.Drawing.Point(7, 42);
+            this.btnReglaFalsaU1.Name = "btnReglaFalsaU1";
+            this.btnReglaFalsaU1.Size = new System.Drawing.Size(75, 23);
+            this.btnReglaFalsaU1.TabIndex = 1;
+            this.btnReglaFalsaU1.Text = "Regla Falsa";
+            this.btnReglaFalsaU1.UseVisualStyleBackColor = true;
+            // 
+            // btnTangenteU1
+            // 
+            this.btnTangenteU1.Location = new System.Drawing.Point(7, 71);
+            this.btnTangenteU1.Name = "btnTangenteU1";
+            this.btnTangenteU1.Size = new System.Drawing.Size(75, 23);
+            this.btnTangenteU1.TabIndex = 2;
+            this.btnTangenteU1.Text = "Tangente";
+            this.btnTangenteU1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 469);
+            this.Controls.Add(this.gbxMetodosU1);
+            this.Controls.Add(this.gbxSalidasU1);
+            this.Controls.Add(this.gbxEntradasU1);
+            this.Name = "Formulario";
+            this.Text = "Análisis Numérico - Bonafede, Marengo";
+            this.gbxEntradasU1.ResumeLayout(false);
+            this.gbxEntradasU1.PerformLayout();
+            this.gbxSalidasU1.ResumeLayout(false);
+            this.gbxSalidasU1.PerformLayout();
+            this.gbxMetodosU1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxEntradasU1;
+        private System.Windows.Forms.GroupBox gbxSalidasU1;
+        private System.Windows.Forms.GroupBox gbxMetodosU1;
+        private System.Windows.Forms.TextBox txtMaxIteracionesU1;
+        private System.Windows.Forms.TextBox txtToleranciaU1;
+        private System.Windows.Forms.TextBox txtXDerechaU1;
+        private System.Windows.Forms.TextBox txtXIzquierdaU1;
+        private System.Windows.Forms.TextBox txtFuncionU1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCantIteracionesU1;
+        private System.Windows.Forms.TextBox txtRaizU1;
+        private System.Windows.Forms.TextBox txtErrorRelatU1;
+        private System.Windows.Forms.TextBox txtConvergeU1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnTangenteU1;
+        private System.Windows.Forms.Button btnReglaFalsaU1;
+        private System.Windows.Forms.Button btnBiseccionU1;
+    }
+}
+
---
+++ Logica/Salida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    class Salida
+    {
+        public bool Converge { get; set; }
+        public int Iteraciones { get; set; }
+        public decimal ErrorRelativo { get; set; }
+        public decimal Raiz { get; set; }
+    }
+}
