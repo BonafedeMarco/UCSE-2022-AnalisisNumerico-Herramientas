e494f90c3f2541532f2d89921837cbeb303f71c7
MarcoBonafede <marcobonafede@hotmail.com>
2022-09-19 20:46:07 -0300

    Modelo de entidades

---
+++ Entidades/U2Salida.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public sealed class U2Salida
+    {
+        public bool _Error { get; set; } // Indica si hubo un error
+        public string _MsjError { get; set; } // Detalle de dicho error
+        public string _Metodo { get; set; } // Método utilizado para la resolución
+        public double[] Resultado { get; set; }
+
+        // Métodos
+        public void AgregarMsjError(string msj)
+        {
+            _Error = true;
+            _MsjError = msj;
+        }
+    }
+}
